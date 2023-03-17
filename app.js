e8e544b28f62bbf35b7922d7b030211b21fe300c
Tausani Ah Chong <58315812+tausani-ah-chong@users.noreply.github.com>
2023-03-18 09:22:36 +1300

    Bot reply to thread & handle history for multiple threads

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 require('dotenv').config();
 const { App, ExpressReceiver } = require('@slack/bolt');
-const axios = require('axios');
+const { Configuration, OpenAIApi } = require("openai");
+
+const openAiConfiguration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(openAiConfiguration);
 
 // For express type http methods
 const receiver = new ExpressReceiver({ signingSecret: process.env.SLACK_SIGNING_SECRET });
@@ -11,26 +17,37 @@ const app = new App({
 });
 
 // Config for ChatGPT
-const MODEL_TYPE = 'text-davinci-003';
 const MAX_TOKENS = 2048;
 const TEMPERATURE = 0.8
-const COMPLETIONS_ENDPOINT = 'https://api.openai.com/v1/completions';
-const CHATGPT_API_KEY = process.env.OPENAI_API_KEY;
+const INITIAL_MESSAGES = [
+  {
+    role: 'user',
+    content: 'Hello, who are you?'
+  },
+  {
+    role: 'assistant',
+    content: 'I am an AI created by OpenAI. How can I help you today?'
+  }
+]
 
-const MODEL_CONFIG = {
-  model: MODEL_TYPE,
+const COMPLETION_MODEL_CONFIG = {
+  model: 'text-davinci-003',
   prompt: '',
   max_tokens: MAX_TOKENS,
-  n: 1,
-  stop: null,
+  top_p: 1,
   temperature: TEMPERATURE,
+  frequency_penalty: 0.0,
+  presence_penalty: 0.6,
 }
 
-const HEADERS = {
-  headers: {
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${CHATGPT_API_KEY}`,
-  }
+const CHAT_COMPLETION_MODEL_CONFIG = {
+  model: 'gpt-3.5-turbo',
+  messages: INITIAL_MESSAGES,
+  max_tokens: MAX_TOKENS,
+  top_p: 1,
+  temperature: TEMPERATURE,
+  frequency_penalty: 0.0,
+  presence_penalty: 0.6,
 }
 
 const ERROR_MESSAGE = 'Unluggy uce, there was an error processing your question. Please try again later.'
@@ -57,14 +74,11 @@ app.command('/askchatgpt', async ({ command, ack, respond }) => {
   console.log('Question:', question);
 
   try {
-    const response = await axios.post(
-      COMPLETIONS_ENDPOINT,
-      {
-        ...MODEL_CONFIG,
-        prompt: question,
-      },
-      HEADERS,
-    );
+    const response = await openai.createCompletion({
+      ...COMPLETION_MODEL_CONFIG,
+      prompt: `The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: ${question}\nAI:`,
+      stop: ["Human:", "AI:"],
+    })
 
     const answer = response.data.choices[0].text.trim();
     
@@ -77,32 +91,68 @@ app.command('/askchatgpt', async ({ command, ack, respond }) => {
   }
 });
 
+// History in memory (Resets when server restarts)
+const messageHistoryByThreadTS = new Map()
+let currentMessageHistory = []
+
 // App mention
 app.event('app_mention', async ({ event, say }) => {
-  const question = event.text;
+  const { text: question, thread_ts: threadTimeStamp, ts: eventTimeStamp } = event;
 
-  console.log('Question:', question);
+  const newMessage = {
+    role: 'user',
+    content: question
+  }
+
+  let messagesForEndpoint;
+  
+  // Handle thread history & messages to send to openAI
+  if (threadTimeStamp != null) {
+    const previousHistory = messageHistoryByThreadTS.get(threadTimeStamp)
+
+    currentMessageHistory = previousHistory
+
+    messagesForEndpoint = [...currentMessageHistory, newMessage]
+  } else {
+    messagesForEndpoint = [...CHAT_COMPLETION_MODEL_CONFIG.messages, newMessage]
+
+    currentMessageHistory = []
+  }
+  
+  currentMessageHistory.push(newMessage);
 
   try {
-    const response = await axios.post(
-      COMPLETIONS_ENDPOINT,
-      {
-        ...MODEL_CONFIG,
-        prompt: question,
-      },
-      HEADERS,
-    );
+    const response = await openai.createChatCompletion({
+      ...CHAT_COMPLETION_MODEL_CONFIG,
+      messages: messagesForEndpoint,
+    })
 
-    const answer = response.data.choices[0].text.trim();
+    const answer = response.data.choices[0].message.content.trim();
 
-    console.log('Answer:', answer);
+    const messageToSave = {
+      role: 'assistant',
+      content: answer
+    }
+    
+    currentMessageHistory.push(messageToSave)
 
-    await say(`${answer}`);
+    // Set thread history
+    if (threadTimeStamp != null) {
+      const previousHistory = messageHistoryByThreadTS.get(threadTimeStamp)
+
+      messageHistoryByThreadTS.set(threadTimeStamp, [...previousHistory, messageToSave])
+    } else {
+      messageHistoryByThreadTS.set(eventTimeStamp, currentMessageHistory)
+    }
+
+    await say({ text: `${answer}`, thread_ts: threadTimeStamp ?? eventTimeStamp });
   } catch (error) {
     console.error('Error fetching ChatGPT response:', error);
-    await say(ERROR_MESSAGE);
+    await say({ text: ERROR_MESSAGE, thread_ts: threadTimeStamp ?? eventTimeStamp });
   }
 });
+
+const saveHistory = (currentHistory, answerToSave) => [...currentHistory, answerToSave]
 
 (async () => {
   await app.start(3000);
